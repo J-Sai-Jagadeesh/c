18fb3c57e2d4ad0f7d0f5a99012a52b02ca21732
J-Sai-Jagadeesh <jagadeeshjsai@gmail.com>
2024-09-30 12:22:49 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+int a,b,c;
+printf("enter any two numbers:");
+scanf("%d%d",&a,&b);    
+c=a+b;
+printf("sum of two are %d",c);
+return 0;
+}
