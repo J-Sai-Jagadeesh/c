70ff6f831bbde6cc0f73c8bb5b06775583fec847
J-Sai-Jagadeesh <jagadeeshjsai@gmail.com>
2024-10-25 11:59:26 +0530

    Create pos or neg.c

---
+++ pos or neg.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+int n;
+printf("enter n:");
+scanf("%d",&n);
+if(n>0)
+{
+    printf("given number is positive");
+}
+else
+{
+    printf("given number is negative");
+}
+    return 0;
+}
